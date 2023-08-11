40deeee7f864a575d31530b0e85f1e6987912fa1
QuiD-0 <wodnd101@naver.com>
2023-08-11 17:41:29 +0900

    변수명 수정

--- src/main/kotlin/com/quid/hawkeye/usecase/E9PayApp.kt
+++ src/main/kotlin/com/quid/hawkeye/usecase/E9PayApp.kt
@@ -7,19 +7,19 @@ import org.springframework.stereotype.Component
 
 @Component
 class E9PayApp(
-    private val e9payDriver: E9PayDriver,
+    private val driver: E9PayDriver,
 ) : App {
     override fun initApp(phone: PhoneType) =
-        with(e9payDriver) {
+        with(driver) {
             init(phone)
             selectLanguage()
             permissionAllow()
         }
 
     override fun getRateList(): List<Rate> =
-        e9payDriver.gotoRatePage()
-            .let { e9payDriver.getRateList() }
+        driver.gotoRatePage()
+            .let { driver.getRateList() }
 
-    override fun closeApp() = e9payDriver.close()
+    override fun closeApp() = driver.close()
 
 }
