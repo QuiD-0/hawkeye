bebb30eb7210ca3d4b87424b863fb6a7c263d440
QuiD-0 <wodnd101@naver.com>
2023-08-14 10:16:48 +0900

    init rate

--- src/main/kotlin/com/quid/hawkeye/domain/Rate.kt
+++ src/main/kotlin/com/quid/hawkeye/domain/Rate.kt
@@ -1,6 +1,17 @@
 package com.quid.hawkeye.domain
 
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
 data class Rate(
-    private val appNane:String,
+    private val appName:String,
+    private val country: String,
+    private val remittanceType: String,
+    private val sendAmount:BigDecimal,
+    private val sendCurrency: String,
+    private val receiveAmount:BigDecimal,
+    private val receiveCurrency: String,
+    private val rate: BigDecimal,
+    private val regDate: LocalDateTime = LocalDateTime.now(),
 ) {
 }
